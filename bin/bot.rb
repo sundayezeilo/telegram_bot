6c755dfa9187d442ed2ed460dd92670db122a3fc
ezeilo-su <ezeilo.su@gmail.com>
2020-05-26 14:44:15 +0100

    Add local time message to the user

--- bin/bot.rb
+++ bin/bot.rb
@@ -5,6 +5,7 @@ require 'httparty'
 require '../lib/scraper.rb'
 require '../lib/api_keys.rb'
 require 'country_lookup'
+require 'time'
 
 def scraper(html)
   JSON.parse(html)
@@ -33,7 +34,16 @@ Telegram::Bot::Client.run(YOUR_TELEGRAM_API_TOKEN) do |bot|
         if weather["cod"] == "404" || weather["message"] == "city not found"
           bot.api.send_message(chat_id: message.chat.id, text: "City not found! Provide a valid city.")
         else
-          bot.api.send_message(chat_id: message.chat.id, text: "It's #{(weather["main"]["temp"] - 272.15).round}°C in #{weather["name"]}, #{Country.with_postal_code[weather["sys"]["country"]]}, with #{weather["weather"].first["description"]}.")
+          chat_id = message.chat.id
+          city = weather["name"]
+          cloud_cond = weather["weather"].first["description"]
+          temp = (weather["main"]["temp"] - 272.15).round
+          time = weather["dt"]
+          local_time = Time.at(time)
+          country = Country.with_postal_code[weather["sys"]["country"]]
+          text = "It's #{temp}°C in #{city}, #{country}, with #{cloud_cond},\r\nLocal time: #{local_time}."
+
+          bot.api.send_message(chat_id: chat_id, text: text)
         end
       end
     else
@@ -42,12 +52,8 @@ Telegram::Bot::Client.run(YOUR_TELEGRAM_API_TOKEN) do |bot|
   end
 end
 
-
-# weather_html = HTTParty.get(url).to_s
-# weather = JSON.parse(weather_html)
-# puts "Description: #{weather["weather"].first.fetch("description")}"
-# puts "Temperature: #{(weather["main"]["temp"] - 272.15).round}°C"
-
 # .match(/\/weather +/)
 # ^[a-zA-Z]+$ #matches only strings that consist of one or more letters only (^ and $ mark the begin and end of a string respectively).
 #{"cod":"404","message":"city not found"}
+
+# {"coord":{"lon":3.75,"lat":6.58},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":304.15,"feels_like":307.27,"temp_min":304.15,"temp_max":304.15,"pressure":1013,"humidity":70},"visibility":8000,"wind":{"speed":4.6,"deg":200},"clouds":{"all":75},"dt":1590498434,"sys":{"type":1,"id":1185,"country":"NG","sunrise":1590470904,"sunset":1590515766},"timezone":3600,"id":2332453,"name":"Lagos","cod":200}
