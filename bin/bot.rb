dca00de33403ab8286275ca0659d19eecf8ac2f0
ezeilo-su <ezeilo.su@gmail.com>
2020-05-26 17:53:37 +0100

    Create  format_message method

--- bin/bot.rb
+++ bin/bot.rb
@@ -5,14 +5,23 @@ require 'httparty'
 require '../lib/scraper.rb'
 require '../lib/api_keys.rb'
 require 'country_lookup'
-require 'time'
 
 def scraper(html)
   JSON.parse(html)
 end
 
-def format_time(t_raw)  
+def format_time(t_raw)
   Time.at(t_raw)
+end
+
+def format_message(weather)
+  city = weather['name']
+  cloud_cond = weather['weather'].first['description']
+  temp = (weather['main']['temp'] - 272.15).round
+  local_time = format_time(weather['dt'])
+  country = Country.with_postal_code[weather['sys']['country']]
+  text = "It's #{temp}°C in #{city}, #{country}, with #{cloud_cond}.\r\nLocal time: #{local_time}."
+  text  
 end
 
 chat_id_log = {}
@@ -20,32 +29,26 @@ chat_id_log = {}
 Telegram::Bot::Client.run(YOUR_TELEGRAM_API_TOKEN) do |bot|
   bot.listen do |message|
     if message.text == '/start'
-      if !chat_id_log[(message.chat.id).to_s]
-        chat_id_log[(message.chat.id).to_s] = message.chat.id
+      unless chat_id_log[message.chat.id.to_s]
+        chat_id_log[message.chat.id.to_s] = message.chat.id
         bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}\r\nEnter /weather <city> or /weather <city>,<country code> to get weather info\r\nSend /end to end the chat. Use /start to start again next time.")
       end
     elsif message.text == '/end'
-      chat_id_log.delete((message.chat.id).to_s)
+      chat_id_log.delete(message.chat.id.to_s)
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
-    elsif message.text.match(/\A\/weather +\w+/)   # matches weather plus one or more spaces at the start of a string, then one or more characters that follows
-      city = message.text.slice("/weather".length, message.text.length).lstrip
+    elsif message.text.match(%r{\A/weather +\w+}) # matches weather plus one or more spaces at the start of a string, then one or more characters that follows
+      city = message.text.slice('/weather'.length, message.text.length).lstrip
       if !city
-        bot.api.send_message(chat_id: message.chat.id, text: "City not provided. Try again!")
+        bot.api.send_message(chat_id: message.chat.id, text: 'City not provided. Try again!')
       else
-        url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+YOUR_OPENWEATHERMAP_API_KEY
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + YOUR_OPENWEATHERMAP_API_KEY
         weather_html = HTTParty.get(url).to_s
         weather = scraper(weather_html)
-        if weather["cod"] == "404" || weather["message"] == "city not found"
-          bot.api.send_message(chat_id: message.chat.id, text: "City not found! Provide a valid city.")
+        if weather['cod'] == '404' || weather['message'] == 'city not found'
+          bot.api.send_message(chat_id: message.chat.id, text: 'City not found! Provide a valid city.')
         else
           chat_id = message.chat.id
-          city = weather["name"]
-          cloud_cond = weather["weather"].first["description"]
-          temp = (weather["main"]["temp"] - 272.15).round
-          local_time = format_time(weather["dt"])
-          country = Country.with_postal_code[weather["sys"]["country"]]
-          text = "It's #{temp}°C in #{city}, #{country}, with #{cloud_cond},\r\nLocal time: #{local_time}."
-
+          text = format_message(weather)
           bot.api.send_message(chat_id: chat_id, text: text)
         end
       end
@@ -54,9 +57,3 @@ Telegram::Bot::Client.run(YOUR_TELEGRAM_API_TOKEN) do |bot|
     end
   end
 end
-
-# .match(/\/weather +/)
-# ^[a-zA-Z]+$ #matches only strings that consist of one or more letters only (^ and $ mark the begin and end of a string respectively).
-#{"cod":"404","message":"city not found"}
-
-# {"coord":{"lon":3.75,"lat":6.58},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":304.15,"feels_like":307.27,"temp_min":304.15,"temp_max":304.15,"pressure":1013,"humidity":70},"visibility":8000,"wind":{"speed":4.6,"deg":200},"clouds":{"all":75},"dt":1590498434,"sys":{"type":1,"id":1185,"country":"NG","sunrise":1590470904,"sunset":1590515766},"timezone":3600,"id":2332453,"name":"Lagos","cod":200}
