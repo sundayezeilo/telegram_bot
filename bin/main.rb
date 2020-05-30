47c2dd1a996ef63fc8ad42bf4b9389c607d0ae48
ezeilo-su <ezeilo.su@gmail.com>
2020-05-31 00:38:47 +0100

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -6,7 +6,6 @@ require 'telegram/bot'
 def run_bot(token)
   Telegram::Bot::Client.run(token) do |bot|
     bot.listen do |message|
-      @id_array.push(message.chat.id) unless @id_array.include?(message.chat.id)
       MessageHandler.new(bot, message).handle_message
     end
   end
