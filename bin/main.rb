a61372b0a6e123077dd62cbd151fd8185cb2ae6f
ezeilo-su <ezeilo.su@gmail.com>
2020-05-30 23:21:59 +0100

    Implement proper OOP

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,20 @@
-require 'nokogiri'
-require 'httparty'
-require 'telegram/bot'
-require 'country_lookup'
-require_relative '../lib/scraper.rb'
+require_relative '../lib/parser.rb'
 require_relative '../lib/api_keys.rb'
 require_relative '../lib/tele_bot.rb'
+require 'telegram/bot'
 
-bot = Telebot.new(TELEGRAM_API_TOKEN)
 
-bot.run
+def run_bot(token)
+  @id_array = []
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      @id_array.push(message.chat.id) unless @id_array.include?(message.chat.id)
+      MessageHandler.new(bot, message).handle_message
+    end
+  end
+end
+
+
+run_bot(TELEGRAM_API_TOKEN)
+
+# @id_array.delete(message.chat.id) if 
--- lib/scraper.rb
+++
@@ -1,7 +0,0 @@
-require 'json'
-
-module Scraper
-  def self.parse_json(html)
-    JSON.parse(html)
-  end
-end
