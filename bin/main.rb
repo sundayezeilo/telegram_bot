e6413f782810fc0eaf5296450755ba1c7ec9446c
ezeilo-su <ezeilo.su@gmail.com>
2020-05-30 23:50:12 +0100

    Fix rubocop bugs in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,6 @@ require_relative '../lib/api_keys.rb'
 require_relative '../lib/tele_bot.rb'
 require 'telegram/bot'
 
-
 def run_bot(token)
   @id_array = []
   Telegram::Bot::Client.run(token) do |bot|
@@ -14,7 +13,6 @@ def run_bot(token)
   end
 end
 
-
 run_bot(TELEGRAM_API_TOKEN)
 
-# @id_array.delete(message.chat.id) if 
+# @id_array.delete(message.chat.id) if
