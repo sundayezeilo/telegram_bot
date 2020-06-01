0852959294e62cb493e9c9387ca4d178962541a6
ezeilo-su <ezeilo.su@gmail.com>
2020-06-01 22:19:05 +0100

    Add vcr gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,3 +96,10 @@ RSpec.configure do |config|
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
 end
+
+require "vcr"
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/vcr"
+  c.hook_into :webmock
+end
--- spec/tele_bot_spec.rb
+++ spec/tele_bot_spec.rb
@@ -2,3 +2,5 @@ require_relative '../lib/parser.rb'
 require_relative '../lib/api_keys.rb'
 require_relative '../lib/tele_bot.rb'
 require 'telegram/bot'
+require "vcr"
+
